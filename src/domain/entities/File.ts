966cbc487d7235b7fa6de91cec5a025053a32cf3
IamIsthill <bercasiocharles14@gmail.com>
2025-08-25 00:28:01 +0800

    feat: converted interface domain entities to class

--- src/domain/entities/File.ts
+++ src/domain/entities/File.ts
@@ -1,20 +1,24 @@
-export interface File {
-  id: string;
-  name: string;
-  mimeType: string;
-  webContentLink: string;
-  webViewLink: string;
-  iconLink: string;
-  size?: string;
-  description?: string;
+export class File {
+  constructor(
+    public id: string,
+    public name: string,
+    public mimeType: string,
+    public webContentLink: string,
+    public webViewLink: string,
+    public iconLink: string,
+    public size?: string,
+    public description?: string
+  ) {}
 }
 
-export interface Folder {
-  id: string;
-  name: string;
-  description?: string;
-  mimeType: string;
-  webContentLink: string;
-  webViewLink: string;
-  iconLink: string;
+export class Folder {
+  constructor(
+    public id: string,
+    public name: string,
+    public mimeType: string,
+    public webContentLink: string,
+    public webViewLink: string,
+    public iconLink: string,
+    public description?: string
+  ) {}
 }
--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,4 +1,3 @@
-export interface User {
-  email: string;
-  password: string;
+export class User {
+  constructor(public email: string, public password: string) {}
 }
